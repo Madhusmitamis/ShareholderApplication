e3f573f41c590af3a990b23462bb3cf8f6851842
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-08-29 18:24:31 +0300

    Shareholder.java code written with security pasword and dependency added to pom.xml.

--- src/main/java/com/phz/ShareholderApplication/Repository/ShareholderRepository.java
+++ src/main/java/com/phz/ShareholderApplication/Repository/ShareholderRepository.java
@@ -1,5 +1,9 @@
 package com.phz.ShareholderApplication.Repository;
 
-public class ShareholderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.phz.ShareholderApplication.Model.Shareholder;
+
+public interface ShareholderRepository extends JpaRepository<Shareholder, Long> {
 
 }
