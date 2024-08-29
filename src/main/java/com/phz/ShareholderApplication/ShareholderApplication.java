881c6da1ff176637bc8e5ad51b3c1abbb0553443
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-08-29 17:22:53 +0300

    Initial commit

---
+++ src/main/java/com/phz/ShareholderApplication/ShareholderApplication.java
@@ -0,0 +1,13 @@
+package com.phz.ShareholderApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareholderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareholderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phz/ShareholderApplication/ShareholderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phz.ShareholderApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
