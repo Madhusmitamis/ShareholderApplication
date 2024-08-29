8c9e6a72745649c56074c0a0ac176050fc18fc21
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-08-29 19:20:21 +0300

    codes added in shareholderController class.

--- src/main/java/com/phz/ShareholderApplication/Controller/ShareholderController.java
+++ src/main/java/com/phz/ShareholderApplication/Controller/ShareholderController.java
@@ -1,5 +1,24 @@
 package com.phz.ShareholderApplication.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.phz.ShareholderApplication.Model.Shareholder;
+import com.phz.ShareholderApplication.Service.ShareholderService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/shareholder")
 public class ShareholderController {
+    @Autowired
+    private ShareholderService shareholderService;
+
+    @PostMapping("/add")
+    public String addShareholder(@RequestBody Shareholder shareholder) {
+        shareholderService.saveShareholder(shareholder);
+        return "New shareholder is added";
+    }
 
 }
--- src/main/java/com/phz/ShareholderApplication/Service/ShareholderServiceImpl.java
+++ src/main/java/com/phz/ShareholderApplication/Service/ShareholderServiceImpl.java
@@ -1,10 +1,12 @@
 package com.phz.ShareholderApplication.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.phz.ShareholderApplication.Model.Shareholder;
 import com.phz.ShareholderApplication.Repository.ShareholderRepository;
 
+@Service
 public class ShareholderServiceImpl implements ShareholderService {
     @Autowired
     private ShareholderRepository shareholderRepository;
